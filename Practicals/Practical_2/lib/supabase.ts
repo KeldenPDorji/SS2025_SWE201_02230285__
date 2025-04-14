05ad30e90167c6d4ff7d432e02cb4eff204dc420
7Kelden Drac <kelden7@drac.kelden7>
2025-04-14 11:11:50 +0600

    [feat]: backtothegame5

---
+++ Practicals/Practical_2/lib/supabase.ts
@@ -0,0 +1,29 @@
+import { AppState } from 'react-native'
+import 'react-native-url-polyfill/auto'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = "https://unzfzmqzfsqdbdzhwuzi.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuemZ6bXF6ZnNxZGJkemh3dXppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwOTEzMTUsImV4cCI6MjA1OTY2NzMxNX0.J9jjoUpUB39W0Y9R0Sn68sfLSdq0fvR6hnRl16V2VPo"
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
+
+// Tells Supabase Auth to continuously refresh the session automatically
+// if the app is in the foreground. When this is added, you will continue
+// to receive `onAuthStateChange` events with the `TOKEN_REFRESHED` or
+// `SIGNED_OUT` event if the user's session is terminated. This should
+// only be registered once.
+AppState.addEventListener('change', (state) => {
+  if (state === 'active') {
+    supabase.auth.startAutoRefresh()
+  } else {
+    supabase.auth.stopAutoRefresh()
+  }
+})
